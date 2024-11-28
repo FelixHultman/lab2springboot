840e051488cd9bb984c4d564568c76254fc98c0e
Felix Hultman <karlfelixhultman@gmail.com>
2024-11-28 23:24:51 +0100

    added user to database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,19 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
+TRUNCATE `user`;
 TRUNCATE category;
 TRUNCATE place;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO `user` (id, username, password, role)
+VALUES (101, 'user1', 'password1', 'USER'),
+       (102, 'user2', 'password2', 'USER'),
+       (103, 'user3', 'password3', 'USER'),
+       (104, 'user4', 'password4', 'ADMIN'),
+       (105, 'user5', 'password5', 'USER'),
+       (106, 'user6', 'password6', 'USER'),
+       (107, 'user7', 'password7', 'USER');
 
 INSERT INTO category (name, symbol, description)
 VALUES ('Park', '🌳', 'Outdoor green spaces.'),
@@ -21,3 +31,4 @@ VALUES ('Central Park', 'A large public park in the city center.', 1, FALSE, POI
        ('Ocean View', 'A scenic spot overlooking the ocean.', 4, FALSE, POINT(37.774929, -122.419416), 105),
        ('Art Museum', 'A modern art museum with a rich collection.', 5, FALSE, POINT(48.856613, 2.352222), 106),
        ('Hidden Gem', 'A secret location known only to locals.', 1, TRUE, POINT(59.329323, 18.068581), 107);
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,15 @@
 DROP TABLE IF EXISTS place;
 DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS `user`;
 
+
+CREATE TABLE IF NOT EXISTS `user`
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role     ENUM ('USER', 'ADMIN') DEFAULT 'USER'
+);
 
 CREATE TABLE IF NOT EXISTS category
 (
@@ -16,10 +25,12 @@ CREATE TABLE IF NOT EXISTS place
     name        VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     category_id INT          NOT NULL,
-    is_private   BOOLEAN      NOT NULL DEFAULT FALSE,
-    created_at   TIMESTAMP             DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at   TIMESTAMP             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    is_private  BOOLEAN      NOT NULL DEFAULT FALSE,
+    created_at  TIMESTAMP             DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at  TIMESTAMP             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     coordinates POINT        NOT NULL,
     user_id     INT          NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES category (id)
+    FOREIGN KEY (category_id) REFERENCES category (id),
+    FOREIGN KEY (user_id) REFERENCES `user` (id)
 );
+
